57d94ef4232f2f90b87b6e336959f10051659fec
mercymih <mihretJesus@yahoo.com>
2024-09-04 20:07:42 +0300

    created AI historical site

---
+++ src/index.js
@@ -0,0 +1,33 @@
+function displayHistoricalSites(response) {
+  new Typewriter("#historicalSite", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 10,
+  });
+}
+
+function generateHistoricalSites(event) {
+  event.preventDefault();
+
+  let userCountries = document.querySelector("#user-countries");
+
+  let apiKey = "c71e44b46oa0t62f8d332460f8567af3";
+
+  let context =
+    "You are the best travel expert. Your mission is to suggest short suggestion about a must-visit historical site and separate each place with a <br />. Sign the places with 'Shecodes AI' inside a <strong> element only at the end and NOT at the beginning";
+
+  let prompt = `What are some must-visit historical sites in ${userCountries.value} for history buffs?`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let historicalSiteElement = document.querySelector("#historicalSite");
+  historicalSiteElement.classList.remove("hidden");
+  historicalSiteElement.innerHTML = `<div class="generating">⏳ Generating must-visit historical sites in  ${userCountries.value}</div>`;
+
+  axios.get(apiUrl).then(displayHistoricalSites);
+}
+
+let historicalSiteFormElement = document.querySelector(
+  "#historical-site-generator-form"
+);
+historicalSiteFormElement.addEventListener("submit", generateHistoricalSites);
